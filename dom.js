1fd093774de102851f90315771c6c1e0ce67460b
bp <brian.john.porter@gmail.com>
2012-08-09 20:32:24 -0400

    Updated code to the jquery pattern more

--- dom.js
+++ dom.js
@@ -1,33 +1,32 @@
-/*
-    dom.js - 
-        a. $(document).ready(functionName);
-        b. Selectors 
-            1) ID - $('#id')
-            2) Class - $('.class')
-            3) Tag Name(s) - $('div') or $('div,a,pre,span...')
-        c. CSS 
-            1) css('attributeName'), returns the value of the given css style attribute
-        d. HTML
-            1) attr('attributeName'), returns the value of the given html tag attribute
-*/          
 var $ = function(param) {
     return $.fn.init(param);
 };
 $.fn = {
-    htmlElementsList: null,
+    length: 0,
     validHtmlTags: ['button', 'input', 'form', 'label', 'article', 'p', 'div', 'header', 'nav', 'ul', 
                 'li', 'a', 'span', 'section', 'body', 'html', 'head', 'h2', 'h1', 'h3', 'h4', 'h5', 
                 'dd', 'dl', 'table', 'pre', 'hr'],
+    copyArray: function(arr) {
+        //console.log(arr);
+        if(arr.length && arr.length > 0) {            
+            for(var i = 0; i < arr.length; i++) {
+                this[i] = arr[i];
+            }
+        } else {
+            this[0] = arr;
+        }
+    },
     selector: function(param) {
         var self = this;
         var id, className, isValidTag;
+
         //run css selector engine
         if (param.charAt(0) === '#') { //Looking for an id, eg: $('#container')
             id = param.substr(1, param.length);
-            this.htmlElementsList = document.getElementById(id);
+            this.copyArray(document.getElementById(id));
         } else if (param.charAt(0) === '.') { //Looking for class, eg: $('.current')
             className = param.substr(1, param.length);
-            this.htmlElementsList = document.getElementsByClassName(className);
+            this.copyArray(document.getElementsByClassName(className));
         } else { //Looking for tag, or tags, eg: $('div') or $('a,span,p')
             isValidTag = false;
             var tempList = [];
@@ -48,10 +47,11 @@ $.fn = {
                 });                
             });
             
-            this.htmlElementsList = tempList;
+            this.length = tempList.length;
+            this.copyArray(tempList);
         }
         
-        return this.ret();
+        return this;
     },
     init: function(param) {        
         if (typeof (param) === 'string') {
@@ -60,13 +60,9 @@ $.fn = {
             var documentReady = false;
             document.ready = function (callback) {
                 if(document.readyState === "complete") {
-                      console.log('already complete');
                       documentReady = true;
-                      console.log(typeof(callback));
-                      console.log(callback);
                       callback();
                 } else {
-                  console.log('else');
                   //window.addEventListener("onload",callback, false); //IE
                   document.addEventListener("DOMContentLoaded", callback, false); //firefox, chrome
                 }
@@ -76,40 +72,43 @@ $.fn = {
         }
     },
     show: function () {
-		var i = 0;
-		for (i; i < this.htmlElementsList.length; i++) {
-			this.htmlElementsList[i].style.display = 'block';
+		for (var i = 0; i < this.length; i++) {
+			this[i].style.display = 'block';
 		}
-		return this.ret();
+		return this;
 	},
     hide: function () {
-		var i = 0;
-		for (i; i < this.htmlElementsList.length; i++) {
-			this.htmlElementsList[i].style.display = 'none';
+        console.log('len = ' + this.length);
+		for (var i = 0; i < this.length; i++) {
+            //console.log(i + ', ' + this[i]);
+            //console.log('\n\n');
+			this[i].style.display = 'none';
 		}
-		return this.ret();
+		return this;
 	},
     attr: function (attribute, value) {
 		if (value === undefined) {
-			return this.htmlElementsList[0].getAttribute(attribute);
+			return this[0].getAttribute(attribute);
 		}
-		return this.ret();
+        return this;
 	},
 	css: function (attribute, value) {
 		if (value === undefined) {
-			return this.htmlElementsList[0].style[attribute];
+			return this[0].style[attribute];
 		}
-		return this.ret();
-	},
-    ret: function() {
-        this.htmlElementsList.htmlElementsList = this.htmlElementsList;
-        this.htmlElementsList.hide = this.hide;
-		this.htmlElementsList.attr = this.attr;
-		this.htmlElementsList.css = this.css;
-		this.htmlElementsList.ret = this.ret;
-		this.htmlElementsList.show = this.show;
-        return this.htmlElementsList;
-    }
+		return this;
+	}
 };
 
-
+/*
+   dom.js - 
+        a. $(document).ready(functionName);
+        b. Selectors 
+            1) ID - $('#id')
+            2) Class - $('.class')
+            3) Tag Name(s) - $('div') or $('div,a,pre,span...')
+        c. CSS 
+            1) css('attributeName'), returns the value of the given css style attribute
+        d. HTML
+            1) attr('attributeName'), returns the value of the given html tag attribute
+*/          
